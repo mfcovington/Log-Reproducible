b9f2d1b5355eef4568da366e86a17c88f6aa6bb1
Mike Covington <mfcovington@gmail.com>
2014-03-21 10:34:18 -0700

    Add git info (if available) to archive

--- Log/Reproducible.pm
+++ Log/Reproducible.pm
@@ -15,11 +15,6 @@ use POSIX qw(strftime);
 # TODO: Set dir with --reprodir XXX
 # TODO: Add verbose (or silent) option
 # TODO: Standalone script that can be used upstream of any command line functions
-# TODO: Archive version # and/or current git SHA1, if available
-#         git log -n1 --oneline
-#         git diff
-#         git status ?
-#         -- fatal: Not a git repository (or any of the parent directories): .git
 
 sub first_index (&@) {    # From List::MoreUtils v0.33
     my $f = shift;
@@ -35,14 +30,14 @@ sub reproduce {
     $dir = _set_dir($dir);
     make_path $dir;
 
-    my ( $prog, $cmd, $note ) = _parse_command();
+    my ( $prog, $prog_dir, $cmd, $note ) = _parse_command();
     my $repro_file = _set_repro_file( $dir, $prog );
 
     if ( $cmd =~ /\s-?-reproduce\s+(\S+)/ ) {
         my $old_repro_file = $1;
         $cmd = _reproduce_cmd( $prog, $old_repro_file, $repro_file );
     }
-    _archive_cmd( $cmd, $repro_file, $note );
+    _archive_cmd( $cmd, $repro_file, $note, $prog_dir );
 }
 
 sub _set_dir {
@@ -64,10 +59,9 @@ sub _parse_command {
     for (@ARGV) {
         $_ = "'$_'" if /\s/;
     }
-    my $prog = $0;
-    $prog = basename $prog;
+    my ( $prog, $prog_dir ) = fileparse $0;
     my $cmd = join " ", $prog, @ARGV;
-    return $prog, $cmd, $note;
+    return $prog, $prog_dir, $cmd, $note;
 }
 
 sub _get_note {
@@ -104,15 +98,40 @@ sub _reproduce_cmd {
 }
 
 sub _archive_cmd {
-    my ( $cmd, $repro_file, $note ) = @_;
+    my ( $cmd, $repro_file, $note, $prog_dir ) = @_;
+    my ( $gitcommit, $gitstatus, $gitdiff ) = _git_info($prog_dir);
+
     open my $repro_fh, ">", $repro_file;
     say $repro_fh $cmd;
-    if ( defined $note ) {
-        my @note_lines = split /\n/, $note;
-        say $repro_fh "#NOTE: $_" for @note_lines;
-    }
+    _add_archive_comment( "NOTE",      $note,      $repro_fh );
+    _add_archive_comment( "GITCOMMIT", $gitcommit, $repro_fh );
+    _add_archive_comment( "GITSTATUS", $gitstatus, $repro_fh );
+    _add_archive_comment( "GITDIFF",   $gitdiff,   $repro_fh );
     close $repro_fh;
     say STDERR "Created new archive: $repro_file";
+}
+
+sub _git_info {
+    my $prog_dir = shift;
+    return if `which git` eq '';
+
+    my $gitbranch = `cd $prog_dir; git rev-parse --abbrev-ref HEAD 2>&1;`;
+    return if $gitbranch =~ /fatal: Not a git repository/;
+    chomp $gitbranch;
+
+    my $gitlog    = `cd $prog_dir; git log -n1 --oneline;`;
+    my $gitcommit = "$gitbranch $gitlog";
+    my $gitstatus = `cd $prog_dir; git status --short;`;
+    my $gitdiff   = `cd $prog_dir; git diff;`;
+    return $gitcommit, $gitstatus, $gitdiff;
+}
+
+sub _add_archive_comment {
+    my ( $title, $comment, $repro_fh ) = @_;
+    if ( defined $comment ) {
+        my @comment_lines = split /\n/, $comment;
+        say $repro_fh "#$title: $_" for @comment_lines;
+    }
 }
 
 sub _validate_prog_name {
