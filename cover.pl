f3d8ddb8077c4234d6720d34a7e7ef5501154c5d
Mike Covington <mfcovington@gmail.com>
2014-04-04 17:44:21 -0400

    Change coverage parameters

--- cover.pl
+++ cover.pl
@@ -7,7 +7,7 @@
 use strict;
 use warnings;
 
-$ENV{DEVEL_COVER_OPTIONS} = "-ignore,Build";     # From Module::Build docs
-$ENV{DEVEL_COVER_OPTIONS} = "-ignore,perl5lib";  # From Gabor Szabo's workshop
+# $ENV{DEVEL_COVER_OPTIONS} = "-ignore,Build";     # From Module::Build docs
+# $ENV{DEVEL_COVER_OPTIONS} = "-ignore,perl5lib";  # From Gabor Szabo's workshop
 system("./Build.pl");
 system("./Build testcover");
