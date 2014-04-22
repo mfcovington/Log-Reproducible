049832fbe00a6ee3115e60e364c349c7832c4ce8
Mike Covington <mfcovington@gmail.com>
2014-04-21 19:44:56 -0700

    Update version # and changelog (P:8/8)

--- lib/Log/Reproducible.pm
+++ lib/Log/Reproducible.pm
@@ -14,7 +14,7 @@ use Config;
 # TODO: Standalone script that can be used upstream of any command line functions
 # TODO: Auto-build README using POD
 
-our $VERSION = '0.8.2';
+our $VERSION = '0.9.0';
 
 =head1 NAME
 
