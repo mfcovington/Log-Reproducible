6def93e4e06d84d9021f9f0b0018c66b4a948aea
Mike Covington <mfcovington@gmail.com>
2016-01-25 21:00:46 -0800

    Add copyright and license information

--- lib/Log/Reproducible.pm
+++ lib/Log/Reproducible.pm
@@ -27,6 +27,18 @@ Log::Reproducible - Effortless record-keeping and enhanced reproducibility. Set 
 
 Michael F. Covington <mfcovington@gmail.com>
 
+=head1 COPYRIGHT AND LICENSE
+
+Copyright (C) 2014-2016, Michael F. Covington.
+
+This module is free software; you can redistribute it and/or
+modify it under the same terms as Perl 5.10.0. For more details,
+see the full text of the licenses in the file LICENSES.
+
+This program is distributed in the hope that it will be
+useful, but without any warranty; without even the implied
+warranty of merchantability or fitness for a particular purpose.
+
 =cut
 
 sub _check_for_known_conflicting_modules {
